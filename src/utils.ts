fef399069fe8e11db42d5e495ffe7026ef24a56f
Shashwat Singh <shashwat123student@gmail.com>
2023-09-29 12:00:59 +0530

    Added my database

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgres://fluxlfgv:z1QnzZmyd3bAzGkNxQWGAZ03BPmLifeE@tai.db.elephantsql.com/fluxlfgv");
     await client.connect();
     return client;
 }
